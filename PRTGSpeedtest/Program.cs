db2b150e91bb7b93716750df290472b6938b7083
Dana Harrison <dana@dharrison.ca>
2021-06-14 20:43:17 -0400

    Now using fast-cli from Node

--- PRTGSpeedtest/Program.cs
+++ PRTGSpeedtest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -22,24 +23,33 @@ namespace PRTGSpeedtest
             {
                 StartInfo = new ProcessStartInfo
                 {                    
-                    FileName = ConfigurationSettings.AppSettings["exePath"],
-                    Arguments = "--accept-license",
-                    UseShellExecute = false,
+                    //FileName = ConfigurationSettings.AppSettings["exePath"],
+                    FileName = "cmd.exe",
+                    Arguments = "/C fast --upload --single-line",
+                    UseShellExecute = false,                    
                     RedirectStandardOutput = true,
-                    CreateNoWindow = true
+                    CreateNoWindow = true,
                 }
             };
 
-            p.Start();
+            p.Start();           
+
             while (!p.StandardOutput.EndOfStream)
             {
-                stResult.Add(p.StandardOutput.ReadLine());
+                stResult.Add(p.StandardOutput.ReadLine());                
             }
 
+            string[] fastOut = stResult[stResult.Count - 1].Split('/');
+            string download = fastOut[0];
+            string upload = fastOut[1];
+
+            upload = upload.Substring(upload.IndexOf("Mbps") - 4, 4).Trim();
+            download = download.Substring(download.IndexOf("Mbps") - 4, 4).Trim();
+
             var prtgResultList = new List<PrtgResult>();
-            prtgResultList.Add(new PrtgResult { channel = "Latency (ms)", value = stResult[5].Split(':')[1].Split(new string[] { "ms" }, StringSplitOptions.None)[0].Trim().Split('.')[0] });
-            prtgResultList.Add(new PrtgResult { channel = "Download (Mbps)", value = stResult[7].Split(':')[1].Split(new string[] { "Mbps" }, StringSplitOptions.None)[0].Trim().Split('.')[0] });
-            prtgResultList.Add(new PrtgResult { channel = "Upload (Mbps)", value = stResult[9].Split(':')[1].Split(new string[] { "Mbps" }, StringSplitOptions.None)[0].Trim().Split('.')[0] });
+            //prtgResultList.Add(new PrtgResult { channel = "Latency (ms)", value = stResult[5].Split(':')[1].Split(new string[] { "ms" }, StringSplitOptions.None)[0].Trim().Split('.')[0] });
+            prtgResultList.Add(new PrtgResult { channel = "Download", value = download, unit = "custom", customunit = "Mbps" });
+            prtgResultList.Add(new PrtgResult { channel = "Upload", value = upload, unit = "custom", customunit = "Mbps" });
             var jsonResult = new PrtgRoot
             {
                 prtg = new Prtg
@@ -75,6 +85,11 @@ namespace PRTGSpeedtest
             prtgResultList.Add(new PrtgResult { channel = "Ads Blocked", value = adsBlocked.ToString() });
 
             Console.Write(JsonSerializer.Serialize(jsonResult));
+            if(File.Exists(ConfigurationSettings.AppSettings["outPath"]))
+            {
+                File.Delete(ConfigurationSettings.AppSettings["outPath"]);
+            }
+            File.WriteAllText(ConfigurationSettings.AppSettings["outPath"], JsonSerializer.Serialize(jsonResult));
         }        
     }
 
@@ -82,6 +97,8 @@ namespace PRTGSpeedtest
     {
         public string channel { get; set; }
         public string value { get; set; }
+        public string unit { get; set; }
+        public string customunit { get; set; }
     }
 
     public class Prtg
